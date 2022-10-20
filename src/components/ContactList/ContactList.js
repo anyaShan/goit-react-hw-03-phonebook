a72b5357850a9b032793a5939b95c4b076d60ab1
anya.shan <anya.shanaida@yahoo.com>
2022-10-20 18:43:51 +0300

    to do ContactItem

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,19 +1,19 @@
 import PropTypes from 'prop-types';
-
-import { List, Item } from './ContactList.styled';
+import { ContactItem } from 'components/ContactItem/ContactItem';
+import { List } from './ContactList.styled';
 
 export const ContactList = ({ contacts, deleteContact }) => {
   return (
     <List>
       {contacts.map(({ id, name, number }) => (
-        <Item key={id}>
-          <p>
-            {name}: {number}
-          </p>
-          <button type="button" onClick={() => deleteContact(id)}>
-            Delete
-          </button>
-        </Item>
+        <li key={id}>
+          <ContactItem
+            key={id}
+            name={name}
+            number={number}
+            deleteContact={() => deleteContact(id)}
+          />
+        </li>
       ))}
     </List>
   );
--- src/components/ContactList/ContactList.styled.js
+++ src/components/ContactList/ContactList.styled.js
@@ -6,24 +6,24 @@ export const List = styled.ul`
   gap: 15px;
 `;
 
-export const Item = styled.li`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 10px;
-  border-radius: 5px;
-  color: white;
+// export const Item = styled.li`
+//   display: flex;
+//   align-items: center;
+//   justify-content: space-between;
+//   padding: 10px;
+//   border-radius: 5px;
+//   color: white;
 
-  background-color: rgb(197, 194, 194);
-  box-shadow: rgb(0 0 0 / 20%) 0px 2px 1px -1px,
-    rgb(0 0 0 / 14%) 0px 1px 1px 0px, rgb(0 0 0 / 12%) 0px 1px 3px 0px;
+//   background-color: rgb(197, 194, 194);
+//   box-shadow: rgb(0 0 0 / 20%) 0px 2px 1px -1px,
+//     rgb(0 0 0 / 14%) 0px 1px 1px 0px, rgb(0 0 0 / 12%) 0px 1px 3px 0px;
 
-  font-weight: 600;
-  font-size: 12px;
-  line-height: 1.9;
-  letter-spacing: 0.06em;
+//   font-weight: 600;
+//   font-size: 12px;
+//   line-height: 1.9;
+//   letter-spacing: 0.06em;
 
-  button {
+/* button {
     display: flex;
     justify-content: center;
     align-items: center;
@@ -49,5 +49,5 @@ export const Item = styled.li`
     &:hover {
       background-color: rgb(52, 162, 212);
     }
-  }
-`;
+  } */
+// `;
